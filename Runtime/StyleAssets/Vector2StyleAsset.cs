0021cc3afbaf129e007d9c7343bc5af3b9f3ed30
VladV <vanifatov.vlad@yandex.ru>
2023-06-30 16:28:50 +0400

    Add RectTransform anchoredPosition style

---
+++ Runtime/StyleAssets/Vector2StyleAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CodeWriter.StyleComponents.StyleAssets
+{
+    [CreateAssetMenu(menuName = "Style Components/Assets/Vector2 Style Asset", order = 400)]
+    public sealed class Vector2StyleAsset : StyleAsset<Vector2>
+    {
+    }
+}
---
+++ Runtime/Styles/RectTransformAnchoredPositionStyle.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWriter.StyleComponents
+{
+    using StyleAssets;
+    using UnityEngine;
+
+    [RequireComponent(typeof(RectTransform))]
+    [AddComponentMenu("Style Components/RectTransform Anchored Position Style")]
+    public class RectTransformAnchoredPositionStyle : Style<RectTransform, Vector2, Vector2StyleAsset>
+    {
+        protected override void Apply(RectTransform target, Vector2 value) => target.anchoredPosition = value;
+    }
+}
