809f06ac1380b0c2b91ccf92ed0f0f5093efb752
VladV <vanifatov.vlad@yandex.ru>
2023-06-30 16:36:17 +0400

    Add RectTransform Size style

---
+++ Runtime/Styles/RectTransformSizeStyle.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWriter.StyleComponents
+{
+    using StyleAssets;
+    using UnityEngine;
+
+    [RequireComponent(typeof(RectTransform))]
+    [AddComponentMenu("Style Components/RectTransform Size Style")]
+    public class RectTransformSizeStyle : Style<RectTransform, Vector2, Vector2StyleAsset>
+    {
+        protected override void Apply(RectTransform target, Vector2 value) => target.sizeDelta = value;
+    }
+}
